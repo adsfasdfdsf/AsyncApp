aea3904754bb1d73d9971622d1801fdc5c023186
sadfad <alfhcwl@yandex.ru>
2026-03-09 00:48:29 +0300

    init

---
+++ AsyncTask/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace AsyncTask;
+
+public class Program 
+{
+    public static void Main(string[] args)
+    {
+        
+    }
+}
